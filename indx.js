2e1538232181a7afe334f3391e4c8e493045eb6e
florian <flodal974@gmail.com>
2022-12-09 15:07:26 +0100

    ajout dés dans dossier

--- indx.js
+++ indx.js
@@ -7,22 +7,18 @@ init();
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
     if(gamePlaying) {
-        // 1. Random number
         var dice = Math.floor(Math.random() * 6) + 1;
 
-        //2. Display the result
         var diceDOM = document.querySelector('.dice');
         diceDOM.style.display = 'block';
-        diceDOM.src = 'dice-' + dice + '.png';
+        diceDOM.src = 'dés//dice-' + dice + '.png';
 
 
-        //3. Update the round score IF the rolled number was NOT a 1
         if (dice !== 1) {
-            //Add score
             roundScore += dice;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
         } else {
-            //Next player
+
             nextPlayer();
         }
     }    
@@ -31,13 +27,10 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
     if (gamePlaying) {
-        // Add CURRENT score to GLOBAL score
         scores[activePlayer] += roundScore;
 
-        // Update the UI
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-        // Check if player won the game
         if (scores[activePlayer] >= 100) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
@@ -45,7 +38,6 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
             gamePlaying = false;
         } else {
-            //Next player
             nextPlayer();
         }
     }
@@ -53,7 +45,6 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 
 
 function nextPlayer() {
-    //Next player
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
 
